bcb1e71c06a51571459fa97efcbb8aa6ce8350e2
SilentPrevail <christiankohl97@yahoo.com>
2018-05-02 17:09:12 +0100

    Transport class skeleton added
    Not yet functional

---
+++ Assets/Scripts/Transport.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class Transport : Purchasable {
+
+    int numberOfHouses;
+
+
+    public void payRent(Player player){
+        //TODO update this method!
+        //int i = player.getTransports
+        player.spends(rent[0]);
+    }
+
+}
