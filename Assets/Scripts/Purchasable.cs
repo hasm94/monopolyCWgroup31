6d37ee9f17d89b5f57a65bb2e6cb0f27220a2050
alex-hickes <36962001+alex-hickes@users.noreply.github.com>
2018-05-02 18:40:06 +0100

    Added method to Purchasble
    Added method hasOwner

--- Assets/Scripts/Purchasable.cs
+++ Assets/Scripts/Purchasable.cs
@@ -1,73 +1,124 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System;
 using System.Collections;
 
 public class Purchasable : Tile {
 
+	//Variables applicable to all different properties
 
-    //Variables applicable to all different properties
-    private String name;
+	private String name;
 	private Tile[] NextTile;
-    private int propertyCost;
-    private Player owner;
-    private int rent[];
-    private int mortgage;
-    private boolean isMortgaged;
-    private Colour colour;
+	private int propertyCost;
+	private Player owner;
+	private int rent;
+	private int mortgage;
+	private boolean isMortgaged;
+	private Colour colour;
 
 	// Use this for initialization
-	void Start (String n, Tile[] nt, int c, Colour col)
-    : base(n, nt) {
-        name = n;
-        NextTile = nt;
-        cost = c;
-        owner = null;
-        colour = col;
+
+	void Start (String n, Tile[] nt, int c, Colour col) : base(n) {
+
+		name = n;
+		NextTile = nt;
+		cost = c;
+		owner = null;
+		colour = col;
+
 	}
+
+
 
 	// Update is called once per frame
+
 	void Update () {
+
+
 
 	}
 
-    //Returns the name of the property
-    public String getName(){
-        return name;
-    }
 
-    public Tile[] getNextTile(){
-        return NextTile;
-    }
 
-    //Is run when a player wants to own a property, checks that the playre has enough on their balance
-    public void getsBoughtBy(Player player){
-        if(player.getBalance() > propertyCost && owner == null){
-            player.spends(cost);
-            owner = player;
-            player.addProperty(this);
-        } else {
-            System.Write("Player doesn't have enough money to buy this property");
-        }
-    }
+	//Returns the name of the property
 
-    public void sellsProperty(){
-        owner = null;
-        owner.earns(propertyCost);
-    }
+	public String getName(){
 
-    //Mortgages the property, adds to the players' balance
-    public void mortgageProp(){
-        owner.earns(mortgage);
-        isMortgaged = True;
-    }
+		return name;
 
-    //Mortgage is returns
-    public void payOffMortgage(){
-        owner.spends(mortgage);
-        isMortgaged = false;
-    }
+	}
 
-    //Abstract method to make sure all of the subclasses have a getRent method
-    public abstract void payRent(Player player);
+
+
+	public Tile[] getNextTile(){
+
+		return NextTile;
+
+	}
+
+	public bool hasOwner() {
+		if (owner != null) {
+			return true;
+		} else {
+			return false;
+		}
+			
+	}
+
+	//Is run when a player wants to own a property, checks that the playre has enough on their balance
+
+	public void getsBoughtBy(Player player){
+
+		if(player.getBalance() > propertyCost && owner == null){
+
+			player.spends(cost);
+
+			owner = player;
+
+			player.addProperty(this);
+
+		} else {
+
+			System.Write("Player doesn't have enough money to buy this property");
+
+		}
+
+	}
+
+	public void sellsProperty(){
+
+		owner = null;
+
+		owner.earns(propertyCost);
+
+	}
+
+
+	//Mortgages the property, adds to the players' balance
+
+	public void mortgageProp(){
+
+		owner.earns(mortgage);
+
+		isMortgaged = True;
+
+	}
+
+
+
+	//Mortgage is returns
+
+	public void payOffMortgage(){
+
+		owner.spends(mortgage);
+
+		isMortgaged = false;
+
+	}
+
+	//Abstract method to make sure all of the subclasses have a getRent method
+
+	public abstract void payRent(Player player);
+
+
 
 }
